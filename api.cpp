e1e5dbc3e621b6604020c7cc74d00d051ebc0e65
Riley Blackwell <rileyblackwell5@gmail.com>
2022-09-19 18:06:00 -0400

    api.cpp complete

--- api.cpp
+++ api.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <list>
-#include <cassert>
+#include <cassert> 
 #include "json.hpp"
 
 using nlohmann::json;
@@ -17,6 +17,8 @@ public:
     // Returns the queue position for the added student.
     int add_queue_position(const string &uniqname, const string &location);
 
+    // EFFECTS: Returns the student at the front of the queue as a json formatted string.
+    // If the queue is empty then an empty string is returned.
     string get_first_queue_position() const;
 
     string get_all_queue_positions() const;
@@ -46,7 +48,19 @@ int main() {
     for (int i = 0; i < 8; ++i) {
       cin >> headers;
     }
-    if (request_method == "POST" && api_request == "/api/queue/tail/") {
+    
+    if (request_method == "GET" && api_request == "/api/") {
+      json supported_urls = {
+        {"queue_head_url", "http://localhost/queue/head/"},
+        {"queue_list_url", "http://localhost/queue/"},
+        {"queue_tail_url", "http://localhost/queue/tail/"}
+      };
+      string supported_urls_str = supported_urls.dump(4) + "\n";
+      cout << "HTTP/1.1 200 OK\nContent-Type: application/json; charset=utf-8\nContent-Length: 160\n\n"
+           << supported_urls_str;
+    }
+    
+    else if (request_method == "POST" && api_request == "/api/queue/tail/") {
       cin >> request_body;
       request_body["position"] = ohqueue.add_queue_position(request_body["uniqname"], request_body["location"]);
       string response_body = request_body.dump(4) + "\n";
@@ -54,6 +68,30 @@ int main() {
       cout << "HTTP/1.1 201 Created\nContent-Type: application/json; charset=utf-8\nContent-Length: "
            << body_length << "\n\n" << response_body;
     }
+    
+    else if (request_method == "GET" && api_request == "/api/queue/head/") {
+      string response_body = ohqueue.get_first_queue_position(); 
+      if (!response_body.empty()) {
+        int body_length = response_body.length();
+        cout << "HTTP/1.1 200 OK\nContent-Type: application/json; charset=utf-8\nContent-Length: "
+             << body_length << "\n\n" << response_body;
+      } else {
+        cout << "HTTP/1.1 400 Bad Request\nContent-Type: application/json; charset=utf-8\nContent-Length: 0\n\n"; 
+      } 
+    }
+    
+    else if (request_method == "GET" && api_request == "/api/queue/") {
+      string response_body = ohqueue.get_all_queue_positions();
+      if (!response_body.empty()) { 
+        int body_length = response_body.length();
+        cout << "HTTP/1.1 200 OK\nContent-Type: application/json; charset=utf-8\nContent-Length: "
+             << body_length << "\n\n" << response_body;
+      } else {
+        cout << "HTTP/1.1 200 OK\nContent-Type: application/json; charset=utf-8\nContent-Length: 40"
+             << response_body;
+      }
+    }
+    
     else if (request_method == "DELETE" && api_request == "/api/queue/head/") {
       if (ohqueue.delete_first_queue_position()) {
         cout << "HTTP/1.1 204 No Content\nContent-Type: application/json; charset=utf-8\nContent-Length: 0\n\n";
@@ -61,24 +99,14 @@ int main() {
         cout << "HTTP/1.1 400 Bad Request\nContent-Type: application/json; charset=utf-8\nContent-Length: 0\n\n";
       }
     }
-    else if (request_method == "GET" && api_request == "/api/") {
-
-    }
-    else if (request_method == "GET" && api_request == "api/queue/head/") {
-
-    }
-    else if (request_method == "GET" && api_request == "api/queue/") {
-
-    }
+    
     else {
       if (headers != "0") {
         cin >> request_body;
       }
       cout << "HTTP/1.1 400 Bad Request\nContent-Type: application/json; charset=utf-8\nContent-Length: 0\n\n";  
-    }
-    
-  }
-  
+    } 
+  } 
   return 0;
 }
 
@@ -94,11 +122,42 @@ int OHQueue::add_queue_position(const string &uniqname, const string &location) 
 }
 
 string OHQueue::get_first_queue_position() const {
-  assert(false);
+  if (!queue.empty()) {
+    json response = {
+      {"uniqname", queue.front().uniqname},
+      {"location", queue.front().location},
+      {"position", 1}
+    };
+    return response.dump(4) + "\n";  
+  }
+  return "";
 }
 
 string OHQueue::get_all_queue_positions() const {
-  assert(false);
+  if (!queue.empty()) {
+    json response;
+    int queue_size = queue.size();
+    response["count"] = queue_size;
+    json students;
+    int position = 1;
+    for (list<Student>::const_iterator it = queue.begin(); it != queue.end(); ++it) {
+      Student student = *it;
+      json student_object = {
+        {"location", student.location},
+        {"position", position++},
+        {"uniqname", student.uniqname}  
+      };
+      students.push_back(student_object); 
+    }
+    response["results"] = students;
+    return response.dump(4) + "\n";
+  }
+
+  json queue_empty_response = {
+    {"count", 0},
+    {"results", NULL}
+  };
+  return queue_empty_response.dump(4) + "\n";
 }
 
 bool OHQueue::delete_first_queue_position() {
